c63175e43e8150dc3e7fb80efdfc6b9ddd047599
Andrés Buelvas <afroelectrico@gmail.com>
2018-03-28 14:15:13 -0400

    inscripcion lista

--- cliente.py
+++ cliente.py
@@ -1,5 +1,4 @@
 """
-    
     cliente.py
 
     Fecha: /04/2018
@@ -7,16 +6,86 @@
     carnet: 13-10184
     Materia: CI-4835 Redes De Computadoras I
     Proyecto #2: Cliente-Servidor
-
 """
 
+from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
+import socket, threading, time
+
+class Cliente(threading.Thread):
+
+    def __init__(self, cliente):
+        threading.Thread.__init__(self)
+        self.cliente = cliente
+        self.logueado = False
+
+    def login(self):
+        terminar = False
+        while not(terminar):
+            print("1) Loguearse \n2) Registrarse \n0) Salir")
+            opcion = int(input(">>> "))
+            if 0 <= opcion <= 2:
+                if opcion == 1:
+                    user, passw = self.datos()
+                    todo_bien = self.cliente.consultarlogin(user, passw)
+                    if todo_bien:
+                        print("Usted se ha logueado :)\n")
+                        self.logueado = True
+                    else:
+                        print("Usuario o password incorrecto\n")
+
+                elif opcion == 2:
+                    user, passw = self.datos()
+                    todo_bien = self.cliente.consultarRegistro(user)
+                    if not(todo_bien):
+                        listo = self.cliente.inscribirse(user, passw)
+                        print("El usuario " + str(user) + " se ha registrado exitosamente\n")
+                    else:
+                        print("El username "+str(user)+" ya se encuentra en la base de datos\n")
+
+                elif opcion == 0:
+                    terminar = True
+
+    def datos(self):
+        user = input("Introduzca el username: ")
+        passw = input("Introduzca la password: ")
+        return user, passw
+
+    def consola(self):
+        terminar = False
+        while not(terminar):
+            print("1) LISTA_LIBROS \n2) SOLICITUD libro \n0) Salir")
+            opcion = int(input(">>> "))
+            if 0 <= opcion <= 2:
+                if opcion == 1:
+                    print("LISTA_LIBROS ")
+                elif opcion == 2:
+                    print("SOLICITUD libro")
+                else:
+                    terminar = True
+
+    def hacerOperaciones(self):
+        print(self.cliente.add(3, 2))
+        print(self.cliente.system.listMethods())
+
+
+def datosDelServidor():
+    ip = input("Introduzca la direccion ip del servidor: ")
+    return ip
+
+def conectar(ip):
+    cliente = xmlrpc.client.ServerProxy(ip)
+    return cliente
 
 ### Corrida del programa
-cliente = xmlrpc.client.ServerProxy("http://localhost:8000/")
-print(cliente.add(3,2))
-print(cliente.system.listMethods())
+if __name__ == '__main__':
 
+    #ip_server = datosDelServidor()
+    instancia = conectar('http://localhost:8000')
+
+    cliente = Cliente(instancia)
+    cliente.start()
+    cliente.login()
 
 
  
--- servidor.py
+++ servidor.py
@@ -1,5 +1,4 @@
 """
-    
     servidor.py
     
     Fecha: /04/2018
@@ -7,32 +6,99 @@
     carnet: 13-10184
     Materia: CI-4835 Redes De Computadoras I
     Proyecto #2: Cliente-Servidor
-
-
 """
 
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
+import sys, json, os, threading
 
-class Servidor:
+class Servidor(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
 
+class ServidorCentral(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def consolaServidorCentral():
+        terminar = False
+        while not (terminar):
+            print("1) LibrosDescargadosxServidor \n2) Nro. De clientes atendidos \n3) Servidores Caídos \n0) Salir")
+            opcion = int(input(">>> "))
+            if 0 <= opcion <= 3:
+                if opcion == 1:
+                    print("LibrosDescargadosxServidor\n")
+                elif opcion == 2:
+                    print("Nro. De clientes atendidos\n")
+                elif opcion == 3:
+                    print("Servidores Caídos\n")
+                else:
+                    terminar = True
+
+class Cliente():
+
+    # Funcion prueba de xml-rpc
     def add(self, x, y):
         return x + y
 
+    def listaDeLibros(self):
+        pass
+
+    # Verifica si el cliente ya se encuentra en la base de datos
+    def consultarRegistro(self, user):
+        with open('inscripciones.json', 'r') as archivo:
+            info = json.load(archivo)
+
+        for i in info['Registro']:
+            if i['usuario'] == user:
+                return True
+        return False
+
+    def consultarlogin(self, user, passw):
+        with open('inscripciones.json', 'r') as archivo:
+            info = json.load(archivo)
+
+        for i in info['Registro']:
+            if i['usuario'] == user and i['password'] == passw:
+                return True
+        return False
+
+    # Registra el nuevo cliente
+    def inscribirse(self, user, passw):
+        with open('inscripciones.json', 'r+') as f:
+            data = json.load(f)
+            f.seek(0)
+            data["Registro"].append({'usuario': user, 'password': passw})
+            json.dump(data, f, indent=4)
+        return True
+
+# Crea los archivos del servidor
+def crearArchivos():
+    with open('inscripciones.json', 'a+') as f:
+        if os.stat('inscripciones.json').st_size == 0:
+            data = {}
+            data['Registro'] = []
+            json.dump(data, f)
 
 ### Corrida del programa
-server = SimpleXMLRPCServer(("localhost", 8000))
+if __name__ == '__main__':
+    puerto = 8000
+    server = SimpleXMLRPCServer(("localhost", puerto)) #Servidor Central
 
-server.register_instance(Servidor())
-server.register_introspection_functions()
-#server.register_multicall_functions()
-#server.register_function(add)
-server.serve_forever()
-"""
-try:
-      server.serve_forever()
-  except KeyboardInterrerupt:
-      print( Tecla de interrucion recivida. Saliendo")
-      sys.exit(0)  
-"""
+    crearArchivos()
+
+    server.register_instance(Cliente())
+    server.register_introspection_functions()
+    #server.register_multicall_functions()
+    #server.register_function(add)
+    sc = ServidorCentral()
+    sc.start()
+
+    server.serve_forever()
+
+    print("Esperando peticiones por el puerto: "+str(puerto))
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Saliendo')
     
